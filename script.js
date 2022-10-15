d8554562aab09966291f4eea4224ff2877790917
Ученик10 <user10@school.ru>
2022-10-15 16:02:50 +1200

    Мой проект

---
+++ script.js
@@ -0,0 +1,210 @@
+// console.log("Hellow World");
+// const goo = 2
+// let age = +prompt("сколько тебе лет")
+
+// console.log(goo)
+// let str = "Привет "
+// str += "Мир"
+// let roo = true
+// let too = "1"
+// let yoo = too == 1
+// console.log(typeof str)
+// console.log(typeof roo)
+// console.log(typeof yoo)
+// let uoo = 3
+// let ioo = 3
+// uoo = uoo ** ioo
+// console.log(uoo)
+// let ooo = 2
+// let opp = "Геншин"
+// console.log(typeof ooo)
+// console.log(typeof opp)
+// ooo = opp + opp === ooo
+// console.log(typeof ooo)
+// let str = "яблоко"
+//     str1 = "верно"
+//     str2 = "угорь"
+//     str3 = "мимо"
+
+// console.log(str[1] + str1[1] + str2[1] + str3[1] + "!")
+// console.log(str.length)
+// console.log(str.indexOf("К"))
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
+// str = str.replace("яблоко","пицца")
+// console.log(str)
+// console.log(str.slice(1,3))
+// console.log(str)
+// console.log(str.replace("пицца","яблоко").slice(1,2).toUpperCase())
+
+// let foo = 'Парламент'
+//     foo1 = foo.slice(0,1)
+//     foo2 = foo.slice(2,4)
+//     foo3 = foo.slice(5,10)
+// foo4 = foo1 + "о" + foo2 + "о" + foo3 
+// console.log(foo4)
+
+// let age = +prompt( "Сколько вам лет?" )
+// if (age < 18) {
+//     alert( "Вам сюда нельзя" )
+// } else if (age = 18) {
+//     alert( "Вам можно войти" )
+// } else {
+//     console.log( "А вы вообще большой!" )
+// }
+
+// let foo3 = confirm("Будете играть?")
+// if (foo3) {
+//     alert("Вы приходите к большой пещере и слышите какие-то звуки")
+
+//     let foo4 = confirm("Вы пойдёте внутрь?")
+//     if (foo4) {
+//         alert("Вы вошли в пщеру и тут очень темно")
+//         let foo5 = confirm("Зажечь факел?")
+//         if (foo5){
+//             alert("Вы зажгли факел и идёте вглубь пещеры")
+//             alert("Вы идёте...")
+//             alert("...")
+//             alert("...")
+//             alert("Становится жарко")
+//             let foo6 = confirm("Продолжите идти?")
+//             if (foo6) {
+//                 alert("Вы продолжаете идти вглубь пещеры")
+//                 alert("...")
+//                 alert("Вы упали в лаву и умерли")
+//                 alert("Вы проиграли")
+//             } else {
+//                 alert("Вы оборачиваетесь и понимаете что заблудились")
+//                 alert("Пред вами три пути вы сели и начали вспоминать откуда вы пришли")
+//                 alert("Пока вы вспоминали на вас упал сталактит и вы умерли")
+//                 alert("Вы проиграли")
+//             }
+//         } else {
+//             alert("Вы упали в яму и разбились насмерть")
+//             alert("Вы проиграли")
+//         }
+//     } else {
+//         alert("Вы убежали поджав хвост")
+//         alert("Вы проиграли")   
+//     }
+// } else {
+//     alert("Вы отказались от того чтобы играть")
+// }
+
+// let g4 = null
+
+// if (g4) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+// let g5
+
+// if (g5 === false) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+// let foo3 = confirm("Будете играть?")
+// foo3 ? alert("Вы приходите к большой пещере и слышите какие-то звуки"):alert("Вы отказались от того чтобы играть")
+// let foo4 = confirm("Вы пойдёте внутрь?")
+// foo4 ?  alert("Вы вошли в пщеру и тут очень темно"):fo1()
+// let foo5 = confirm("Зажечь факел?")
+// foo5 ? fo2():fo3()
+// let foo6 = confirm("Продолжите идти?")
+// foo6 ? fo4():fo5()
+
+// function fo1() {
+//     alert("Вы убежали поджав хвост");
+//     alert("Вы проиграли");
+// }
+// function fo2() {
+//     alert("Вы зажгли факел и идёте вглубь пещеры")
+//     alert("Вы идёте...")
+//     alert("...")
+//     alert("...")
+//     alert("Становится жарко")
+// }
+// function fo3() {
+//     alert("Вы упали в яму и разбились насмерть")
+//     alert("Вы проиграли")
+// }
+// function fo4() {
+//     alert("Вы продолжаете идти вглубь пещеры")
+//     alert("...")
+//     alert("Вы упали в лаву и умерли")
+//     alert("Вы проиграли")
+// }
+// function fo5() {
+//     alert("Вы оборачиваетесь и понимаете что заблудились")
+//     alert("Пред вами три пути вы сели и начали вспоминать откуда вы пришли")
+//     alert("Пока вы вспоминали на вас упал сталактит и вы умерли")
+//     alert("Вы проиграли")
+// }
+
+
+let foo3 = confirm("Будете играть?")
+let foo4
+let foo5
+let foo6
+let i
+foo3 ? fo7():alert("Вы отказались от того чтобы играть")
+
+
+
+// foo4 ?  alert("Вы вошли в пщеру и тут очень темно"):fo1()
+// let foo5 = confirm("Зажечь факел?")
+// foo5 ? fo2():fo3()
+// let foo6 = confirm("Продолжите идти?")
+// foo6 ? fo4():fo5()
+
+function fo1() {
+    alert("Вы убежали поджав хвост");
+    alert("Вы проиграли");
+}
+function fo2() {
+    alert("Вы зажгли факел и идёте вглубь пещеры")
+    alert("Вы идёте...")
+    alert("...")
+    alert("...")
+    alert("Становится жарко")
+}
+function fo3() {
+    alert("Вы упали в яму и разбились насмерть")
+    alert("Вы проиграли")
+}
+function fo6() {
+    alert("Вы вошли в пщеру и тут очень темно")
+    foo5 = confirm("Зажечь факел?")
+}
+function fo4() {
+    alert("Вы продолжаете идти вглубь пещеры")
+    alert("...")
+    alert("Вы упали в лаву и умерли")
+    alert("Вы проиграли")
+
+}
+function fo5() {
+    alert("Вы оборачиваетесь и понимаете что заблудились")
+    alert("Пред вами три пути вы сели и начали вспоминать откуда вы пришли")
+    alert("Пока вы вспоминали на вас упал сталактит и вы умерли")
+    alert("Вы проиграли")
+}
+function fo7() {
+    alert("Вы приходите к большой пещере и слышите какие-то звуки")
+    i = 1
+}
+
+
+// let foo = +prompt("Дайте число от 1 до 6")
+
+// switch(foo) {
+//     case 1:
+//         console.log(1);
+//         break
+//     case 2:
+//         console.log(2);
+//         break
+//     default:
+//         console.log("Ничего")
+//         }
